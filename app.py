1944cd51b190aca129f73531e016099d626d81be
kewalker <kewalker@uvic.ca>
2018-04-15 19:25:19 -0700

    some more pages

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
 	return ('</br>hello?</br>oh, hello!')
+
+@app.route('/zen')
+def zen():
+	return ('hello from zen')
+
+@app.route('/user/<username>')
+	return ('User {}'.format(username))
+
 
 if __name__ == "__main__":
 	app.run()
